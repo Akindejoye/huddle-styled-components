34074150f0945d3a3fa7f5343f9ad8ad811cf99f
Akindejoye <tydruhill@gmail.com>
2022-02-01 22:01:18 +0100

    Huddle Project

---
+++ src/components/styles/Global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Encode+Sans+Expanded:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,100;1,200&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500&family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,800&display=swap');
+
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.colors.body};
+        color: hsl(192, 100%, 9%);
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.15em;
+        margin: 0;
+    }
+
+    p {
+        opacity: 0.6;
+        line-height: 1.5;
+    }
+
+    img {
+        max-width: 100%;
+    }
+`;
+
+export default GlobalStyles;
